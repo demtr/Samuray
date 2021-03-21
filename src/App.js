495c9d0561c0b61c1ab752d0718bc4a86d6456af
demtr <domian@mail.ru>
2021-03-21 05:03:36 +0300

    L29 was made

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -12,20 +12,18 @@ import Settings from "./components/Settings/Settings";
 function App(p) {
     // const vr = React.version;
     return (
-        <BrowserRouter>
             <div className="App-container">
                 <Header/>
                 {/*Версия React:{vr};*/}
                 <Navbar/>
                 <div className="App-container-content">
-                    <Route path="/dialogs" render={() => <Dialogs dlg={p.dialogs} msg={p.messages}/>}/>
-                    <Route path="/profile" render={() => <Profile pst={p.anyPosts}/>}/>
+                    <Route path="/dialogs" render={() => <Dialogs state={p.state.dialogBlock}/>}/>
+                    <Route path="/profile" render={() => <Profile state={p.state.profileBlock}/>}/>
                     <Route path="/news" component={News}/>
                     <Route path="/music" component={Music}/>
                     <Route path="/settings" component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
