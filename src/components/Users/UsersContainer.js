ffe100351b47adf87a9447f8c80ef3a77656e402
demtr <domian@mail.ru>
2021-04-18 17:22:55 +0300

    L56 ready! Database communication for users moved to container component.

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,6 +1,40 @@
 import {connect} from "react-redux";
 import Users from "./Users";
 import {followAC, getUsersAC, setNumberOfUsersAC, setPageNumberAC, unfollowAC} from "../../redux/usersReducer";
+import React from "react";
+import * as axios from "axios";
+
+class UsersContainer extends React.Component {
+    // Метод componentDidMount вызывается только 1 раз после отрисовки компоненты
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response)=>{
+                this.props.getUsers(response.data.items);
+                this.props.setNumberOfUsers(response.data.totalCount);
+            });
+    }
+
+    goToPage = (page) => {
+        this.props.setPageNumber(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then((response)=>{
+                this.props.getUsers(response.data.items);
+                this.props.setNumberOfUsers(response.data.totalCount);
+            });
+    }
+
+    render() {
+        return <Users users={this.props.users}
+                      totUsers={this.props.totUsers}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onFollow={this.props.onFollow}
+                      onUnFollow={this.props.onUnFollow}
+                      goToPage={this.goToPage}
+        />
+    }
+}
+
 
 let mapStateToProps = (state) => {
     return {
@@ -21,6 +55,4 @@ let mapDispatchToProps = (dispatch) => {
     };
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
