cd7ac1c4a3835493ab26e695cf597702a13fd7ba
demtr <domian@mail.ru>
2021-04-21 01:13:09 +0300

    L60 ready! withRouter wrapper added!

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,14 +3,17 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profileBlockReducer";
+import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+
+        let userId = this.props.match.params.userId || 2;
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+userId)
             .then((response)=>{
                 this.props.setUserProfile(response.data);
             });
-
     }
 
     render() {
@@ -23,4 +26,6 @@ const mapStateToProps = (state) => ({
     profile: state.profileBlock.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+const ProfileContainerWithRouter = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(ProfileContainerWithRouter);
