c176688ef6bf37a03c8a939d439f180f7528648e
demtr <domian@mail.ru>
2021-06-27 04:29:35 +0300

    L94-95 ready! React lazy and github suspense added.

---
+++ src/hoc/withSuspense.js
@@ -0,0 +1,5 @@
+import React, {Suspense} from "react";
+
+export const withSuspense = (Component) => {
+    return (props) => {return <Suspense fallback={<div>Загрузка...</div>}><Component {...props}/></Suspense>};
+}
