ed2cc524f0f2385f5e06eb57bef90dfc62625eab
demtr <domian@mail.ru>
2021-08-20 18:08:28 +0300

    L99 was made! Some refactor.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Pager from "../common/Pager";
 import User from "./User";
+import c from "./Users.module.css";
 
 let Users = (props) => {
 
     // Формирование списка пользователей
     const userList = props.users.map(el => <User user={el} {...props} key={el.id}/>);
-    return <div> <Pager totItems={props.totUsers} {...props}/> {userList} </div>;
+    return <div>
+        <div><Pager totItems={props.totUsers} {...props}/></div>
+        <div>{userList}</div>
+    </div>;
 }
 
 export default Users;
